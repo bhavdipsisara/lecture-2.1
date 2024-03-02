b9a0895bed74990a3b25ab1d280dd2440a722d16
bhavdip sisara <161108566+bhavdipsisara@users.noreply.github.com>
2024-03-02 21:01:43 +0530

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=12,b=6;
+	int c=a+b;
+	printf("%d+%d = %d\n",a,b,c);
+	printf("%d-%d = %d\n",a,b,a-b);
+	printf("%d*%d = %d\n",a,b,a*b);
+	printf("%d/%d = %d\n",a,b,a/b);
+	printf("%d%%d = %d\n",a,b,a%b);
+}
+
